e969ec2cc8c2f2f4b0260e174e7992f126fd5741
Terrence <Terrencew867@gmail.com>
2026-04-30 18:32:29 -0500

    Added animations and new contact css file

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,6 @@
 import react from "react";
+import "../styles/Contact.css";
+
 
 
 
@@ -9,8 +11,14 @@ const Contact = () => {
 return ( 
     <> 
     <h1 className="contactName">Contact</h1>
-    
-    
+    <a href="tel:18645908123">
+        <button className="callButton">Call Me</button>
+        </a>
+        <input type="text" placeholder="Send email" /> 
+    <a href="mailto: Terrencew867@gmail.com"> 
+        <button className="emailBtn">Email</button>
+        </a>
+
     </>
 )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,7 +12,7 @@ return (
     <div className="container1">
     <h4 className="greeting">Hello,</h4>
     <p className="introduction">I'm <span className="mySpan">Terrence</span></p>
-    <p className="role">and I'm a <span className="role2">{roles}<span>|</span></span></p>
+    <p className="role">and I'm a <span className="role2">{roles}<span className="blinker">|</span></span></p>
     </div>
     </div>
     </>
